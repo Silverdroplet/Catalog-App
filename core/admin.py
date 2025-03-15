3336460f2c151cf781cacd2e7a54200dcafd8229
Silverdroplet <ghn5bw@virginia.edu>
2025-03-14 21:14:43 -0400

    added code in admin so that we can verify the psotgre sql is being updated from adding and deleting items

--- core/admin.py
+++ core/admin.py
@@ -1,6 +1,15 @@
 from django.contrib import admin
 from .models import Profile
+from .models import Equipment, EquipmentImage 
 
 @admin.register(Profile)
 class ProfileAdmin(admin.ModelAdmin):
     list_display = ("user", "is_librarian", "joined_date")
+
+@admin.register(Equipment)
+class EquipmentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'identifier', 'status', 'is_available', 'location', 'added_date', 'added_by')
+
+@admin.register(EquipmentImage)
+class EquipmentImageAdmin(admin.ModelAdmin):
+    list_display = ('equipment', 'caption', 'image')
